65dd6c318054382515ba8aadade6529e69367e14
Umar <you@example.com>
2022-09-21 14:07:56 +0500

    test

---
+++ src/components/Properties/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./style";
+
+export const Properties = () => {
+  return (
+  <Container>
+    Properties
+  </Container>
+  );
+};
+
+export default Properties;
---
+++ src/context/properties/reducer.js
@@ -0,0 +1,7 @@
+export const reducer = (state, { type, action }) => {
+  switch (type) {
+    case "plus":
+    default:
+      return state;
+  }
+};
