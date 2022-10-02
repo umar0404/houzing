f4d961c2960a3eb6277a8b4b35341a7bc2b7c7d2
Umar <you@example.com>
2022-10-02 10:07:01 +0500

    house-card ready for using

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import GenCarousel from "../Carousel";
+import HouseCard from "../HouseCard";
 import { Container } from "./style";
 
 export const Home = () => {
   return (
     <Container>
       <GenCarousel />
+      <HouseCard />
     </Container>
   );
 };
